22b4cd9dfeee971fe079e9da785ab8a756399d42
Jakob <jakob.eichberger@gmx.net>
2025-09-19 15:08:41 +0200

    Allow resorting of trains

--- WPF/ViewModels/VehicleTilePanelViewModel.cs
+++ WPF/ViewModels/VehicleTilePanelViewModel.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Windows;
+using GongSolutions.Wpf.DragDrop;
 using Persistence.Ports;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
@@ -9,7 +11,7 @@ using Shell.WPF.Views;
 
 namespace Shell.WPF.ViewModels
 {
-  public class VehicleTilePanelViewModel
+  public class VehicleTilePanelViewModel : IDropTarget
   {
     private readonly IVehicleRepository vehicleRepository;
     private readonly VehicleTileViewFactory vehicleTileViewFactory;
@@ -30,9 +32,34 @@ namespace Shell.WPF.ViewModels
     private void RefreshTiles()
     {
       VehicleViews.Clear();
-      foreach (VehicleTileView vehicleTileView in vehicleRepository.FullTextSearchVehicles(VehicleSearchText.Value).Select(vehicle => vehicleTileViewFactory(vehicle.Id)))
+      foreach (VehicleTileView vehicleTileView in vehicleRepository.FullTextSearchVehicles(VehicleSearchText.Value)
+                                                                   .OrderBy(vehicle => vehicle.Position)
+                                                                   .Select(vehicle => vehicleTileViewFactory(vehicle.Id)))
       {
         VehicleViews.Add(vehicleTileView);
+      }
+    }
+
+    public void DragOver(IDropInfo dropInfo)
+    {
+      if (dropInfo.Data is VehicleTileView && Equals(dropInfo.TargetCollection, VehicleViews))
+      {
+        dropInfo.Effects = DragDropEffects.Move;
+        dropInfo.DropTargetAdorner = DropTargetAdorners.Insert;
+      }
+    }
+
+    public void Drop(IDropInfo dropInfo)
+    {
+      if (dropInfo.Data is VehicleTileView sourceItem && Equals(dropInfo.TargetCollection, VehicleViews))
+      {
+        int insertIndex = dropInfo.InsertIndex;
+        int sourceIndex = VehicleViews.IndexOf(sourceItem);
+
+        if (sourceIndex != -1 && insertIndex != sourceIndex)
+        {
+          VehicleViews.Move(sourceIndex, insertIndex);
+        }
       }
     }
   }
